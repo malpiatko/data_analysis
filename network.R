fcca4fb8950ba6040244dfc9a3d14c3ab0fd673c
ela <elzbieta.siwy@gmail.com>
2015-05-04 22:56:53 +0100

    Output1 and 2 seperated.

--- network.R
+++ network.R
@@ -2,13 +2,34 @@ network <- function(data, H=3) {
   ##Initialising weights
   n.features <- ncol(data$features)
   n <- length(data$class)
-  newclasses <- lapply(data$class, function(x) ifelse(x==0, 1, -1)
-  alpha <- replicate(n.features+1, runif(H, 0, 0.1))
+  ##Initialising weights. We add additional value for the offset weight
+  ## a[i,j] - from hidden i to input
+  alpha <- replicate(n.features+1, runif(H, -0.1, 0.1))
   omega <- runif(H + 1, -0.1, 0.1)
+  weights <- list(level1=alpha, level2=omega)
+  
+  
   pattern.aug <- cbind(rep(1, n), data$features)
-  output <- network.forward(alpha, omega, pattern.aug)
+  output1 <- output.level1(weights$level1, pattern.aug)
+  output2 <- output.level2(weights$level2, cbind(rep(1,n),output1))
   output
 }
+
+##Computes output of first layer for each example
+output.level1 <- function(weights, examples) {
+  t(apply(examples, 1, function(x) output1.example(weights, x)))
+}
+
+##Computed output of first layer for given example
+output1.example <- function(weights, x) {
+  as.vector(apply(weights,1, function(a) sygmoid(a %*% x)))
+}
+
+output.level2 <- function(weights, examples) {
+  apply(examples, 1, function(x) weights %*% x)
+}
+
+
 
 network.forward <- function(alpha, omega, examples) {
   apply(examples, 1, function(x) network.forward.single(alpha, omega, x))
@@ -18,6 +39,10 @@ network.forward.single <- function(alpha, omega, x) {
   hidden <- apply(alpha, 1, function(weight) weight%*% x)
   hidden <- c(1,hidden)
   omega%*%hidden
+}
+
+sygmoid <-function(x) {
+  1/(1+exp(-x))
 }
 
 hyp.tangent <- function(x) {
